6cb4738949726aab7659ff7aff6d1b8e7300a704
julianNot <julian07099@gmail.com>
2024-04-22 21:57:36 -0500

    feature environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  //URL Registro Firebase
+  apiUrlRegister: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAaZnY5zuOqOLoZfm_hzRg2G1jw1G1c5u4'
+};
