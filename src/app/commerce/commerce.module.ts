e1ced0e8ef2b98fc73e1a2b4125e8748a0cf5bfd
julianNot <julian07099@gmail.com>
2024-04-22 21:47:28 -0500

    config module and routes for pages

--- src/app/commerce/commerce.module.ts
+++ src/app/commerce/commerce.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { CommerceRoutingModule } from './commerce.routing.module';
 
 import { LoginComponent } from './pages/login/login.component';
+import { CommerceHomeComponent } from './pages/commerce-home/commerce-home.component';
+import { CartProductsComponent } from './pages/cart-products/cart-products.component';
+import { BannerCommerceComponent } from './components/banner-commerce/banner-commerce.component';
+import { CardEditComponent } from './components/card-edit/card-edit.component';
+import { BillProductsComponent } from './pages/bill-products/bill-products.component';
+import { AdminPanelComponent } from './pages/admin-panel/admin-panel.component';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    CommerceHomeComponent,
+    CartProductsComponent,
+    BannerCommerceComponent,
+    BillProductsComponent,
+    AdminPanelComponent,
+    CardEditComponent
   ],
   imports: [
     CommonModule,
     CommerceRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ]
 })
 export class CommerceModule { }
--- src/app/commerce/commerce.routing.module.ts
+++ src/app/commerce/commerce.routing.module.ts
@@ -2,11 +2,42 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login.component';
+import { CommerceHomeComponent } from './pages/commerce-home/commerce-home.component';
+import { CartProductsComponent } from './pages/cart-products/cart-products.component';
+import { BillProductsComponent } from './pages/bill-products/bill-products.component';
+import { AdminPanelComponent } from './pages/admin-panel/admin-panel.component';
+
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: LoginComponent,
+    data: { type: 'register' }
+  },
+  {
+    path: 'home',
+    component: CommerceHomeComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'cart',
+    component: CartProductsComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'admin',
+    component: AdminPanelComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'bill',
+    component: BillProductsComponent,
+    canActivate: [AuthGuard]
   }
 ]
 
